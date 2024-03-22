f90bd46fc5e48f099a773af934379b2465dfc911
Tom Parker <tom.parker00@gmail.com>
2024-03-22 10:16:47 -0400

    Add pgadmin

---
+++ kubernetes/apps/database/pgadmin/app/config_local.py
@@ -0,0 +1,17 @@
+AUTHENTICATION_SOURCES = ['oauth2', 'internal']
+OAUTH2_AUTO_CREATE_USER = True
+MASTER_PASSWORD_REQUIRED = False
+OAUTH2_CONFIG = [{
+    'OAUTH2_NAME' : 'authentik',
+    'OAUTH2_DISPLAY_NAME' : 'pgadmin SSO',
+    'OAUTH2_CLIENT_ID' : '${AUTHENTIK_CLIENT_ID}',
+    'OAUTH2_CLIENT_SECRET' : '${AUTHENTIK_CLIENT_SECRET}',
+    'OAUTH2_TOKEN_URL' : 'https://authentik.${SECRET_DOMAIN}/application/o/token/',
+    'OAUTH2_AUTHORIZATION_URL' : 'https://authentik.${SECRET_DOMAIN}/application/o/authorize/',
+    'OAUTH2_API_BASE_URL' : 'https://authentik.${SECRET_DOMAIN}/',
+    'OAUTH2_USERINFO_ENDPOINT' : 'https://authentik.${SECRET_DOMAIN}/application/o/userinfo/',
+    'OAUTH2_SERVER_METADATA_URL' : 'https://authentik.${SECRET_DOMAIN}/application/o/pgadmin/.well-known/openid-configuration',
+    'OAUTH2_SCOPE' : 'openid email profile',
+    'OAUTH2_ICON' : 'fa-openid',
+    'OAUTH2_BUTTON_COLOR' : '#2db1fd'
+}]
